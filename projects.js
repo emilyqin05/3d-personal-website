f638f9fbc4362874c51e2a8c5eccb1bafc610dd2
emilyqin05 <123emilyqin@gmail.com>
2024-08-23 21:39:33 -0700

    added projects documents + blender

---
+++ projects.js
@@ -0,0 +1,84 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+
+// Set up the scene, camera, and renderer
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.physicallyCorrectLights = true;
+document.body.appendChild(renderer.domElement);
+
+// Position the camera
+camera.position.set(0, 0.5, 2);
+camera.lookAt(0, 0.1, 0);
+
+
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1.5);
+directionalLight.position.set(10, 10, 10);
+scene.add(directionalLight);
+
+const pointLight = new THREE.PointLight(0xffffff, 1.5);
+pointLight.position.set(-10, 10, 10);
+scene.add(pointLight);
+
+// Handle scroll events to zoom in/out
+window.addEventListener('wheel', (event) => {
+    const zoomSpeed = 0.5; // Adjust this value to control zoom speed
+    if (event.deltaY > 0) {
+        // Scrolling down - zoom in
+		camera.position.y -= zoomSpeed;
+        camera.position.z -= zoomSpeed;
+    } else if (event.deltaY < 0) {
+        // Scrolling up - zoom out
+		camera.position.y += zoomSpeed;
+        camera.position.z += zoomSpeed;
+    }
+
+    // Optional: Clamp the camera's y position to prevent excessive zooming
+    camera.position.y = THREE.MathUtils.clamp(camera.position.y, -2, 15); // Adjust min and max values as needed
+	camera.position.z = THREE.MathUtils.clamp(camera.position.z, -2, 15); // Adjust min and max values as needed
+
+});
+
+// Animation loop
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+animate();
+
+
+// Instantiate a loader
+const loader = new GLTFLoader();
+
+// // Optional: Provide a DRACOLoader instance to decode compressed mesh data
+// const dracoLoader = new DRACOLoader();
+// dracoLoader.setDecoderPath( '/examples/jsm/libs/draco/' );
+// loader.setDRACOLoader( dracoLoader );
+
+// Load a glTF resource
+loader.load('projects.glb', function (gltf) {
+    gltf.scene.traverse(function (node) {
+        if (node.isMesh) {
+            node.castShadow = true;
+            node.receiveShadow = true;
+
+            if (node.material) {
+                node.material.needsUpdate = true;
+            }
+        }
+    });
+	gltf.scene.position.set(0, 0, 0); // Adjust this to the desired position
+    scene.add(gltf.scene);
+}, function (xhr) {
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+}, function (error) {
+    console.log('An error happened', error);
+});
