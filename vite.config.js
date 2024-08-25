7f82eb525c3a94f30f5a22db2283d74a75adf511
emilyqin05 <123emilyqin@gmail.com>
2024-08-24 23:01:43 -0700

    revised workflow + add ./github output

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     base: "/3d-personal-website/",
+    build: {
+        outDir: './github/workflows' // Set the build output directory here
+      }
 })
