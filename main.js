046f66a07fe6d32f2b4bf09496e5a2802dc16e5e
emilyqin05 <123emilyqin@gmail.com>
2024-08-24 22:08:16 -0700

    update three link

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.117.1/build/three.module.js';
+import * as THREE from './node_modules/three/build/three.module.js'; 
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 
